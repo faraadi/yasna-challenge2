3b90d98eb67ac4ccfd2fe7dd1be7bf72b6bb524c
Farhad <faradidev@gmail.com>
2020-08-19 12:35:00 +0430

    basic layout

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './layout.module.css';
+
+export default function Layout(props) {
+	return (
+		<div className={styles.layout}>
+			<div className={styles.wrapper}>
+				{props.children}
+			</div>
+		</div>
+	)
+}
