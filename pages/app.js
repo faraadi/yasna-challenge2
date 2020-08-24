5f4179c536129e3523aa918916bc8145ad72936e
Farhad <faradidev@gmail.com>
2020-08-24 13:57:06 +0430

    moved app into seperate component

--- pages/app.js
+++ pages/app.js
@@ -1,33 +1,5 @@
-import { useEffect } from 'react';
-import { Avatar, ArtistDetails } from 'lib/components/app';
-import { Row, Col } from 'lib/components/common';
-import { Layout } from 'lib/components';
-import { decodeHash } from 'lib/utils';
+import { App } from 'lib/components/app';
 
-export default function Home() {
-	useEffect(() => {
-		const result = decodeHash(window.location.hash);
-		if (result.err) {
-
-		}
-		else if (!result.access_token === null) {
-
-		}
-		else {
-
-		}
-	}, []);
-
-	return (
-		<Layout>
-			<Row>
-				<Col>
-					<Avatar />
-				</Col>
-				<Col>
-					<ArtistDetails />
-				</Col>
-			</Row>
-		</Layout>
-	);
+export default function AppPage() {
+	return <App />
 }
