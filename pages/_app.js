3b04e1c5d8636c0fdfa751acb8e22d60ebc60534
Farhad <faradidev@gmail.com>
2020-08-26 23:31:08 +0430

    added manifest and related configs

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,8 @@
-import Head from 'next/head';
 import '../styles/index.css';
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<meta
-					name='description'
-					content='A Front-End Hiring Challenge for Yasna Team'
-				/>
-				<meta
-					name='keywords'
-					content='Front End, Front-End, Challenge, Hiring Chanllegne, React, Javascript, Nextjs'
-				/>
-				<link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} />
-			</Head>
 			<Component {...pageProps} />
 		</>
 	)
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
 		const initialProps = await Document.getInitialProps(ctx)
 		return { ...initialProps }
@@ -9,7 +9,45 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang='en-US'>
-				<Head />
+				<Head>
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+					<link rel="manifest" href="manifest.json" />
+					<link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} />
+
+					<meta name='application-name' content='Yasna Front-End Hiring Challenge' />
+					<meta name='apple-mobile-web-app-capable' content='yes' />
+					<meta name='apple-mobile-web-app-status-bar-style' content='default' />
+					<meta name='apple-mobile-web-app-title' content='Yasna Front-End Hiring Challenge' />
+					<meta name='description' content='A Front-End Hiring Challenge for Yasna Team' />
+					<meta name='keywords' content='Front End, Front-End, Challenge, Hiring Chanllegne, React, Javascript, Nextjs' />
+					<meta name='format-detection' content='telephone=no' />
+					<meta name='mobile-web-app-capable' content='yes' />
+					<meta name='msapplication-TileColor' content='#05C782' />
+					<meta name='msapplication-tap-highlight' content='no' />
+					<meta name='theme-color' content='#05C782' />
+					<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+
+					<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+					<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+					<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+					<link rel='mask-icon' href='/mask-icon.svg' color='#05C782' />
+					<link rel='shortcut icon' href='/favicon.ico' />
+
+					<meta name='twitter:card' content='Yasna Challenge' />
+					<meta name='twitter:url' content={process.env.NEXT_PUBLIC_BASE_URL} />
+					<meta name='twitter:title' content='Yasna Challenge' />
+					<meta name='twitter:description' content='Yasna Front-End Hiring Challenge' />
+					<meta name='twitter:creator' content='@DavidWShadow' />
+					<meta property='og:type' content='website' />
+					<meta property='og:title' content='Yasna Challenge' />
+					<meta property='og:description' content='Yasna Front-End Hiring Challenge' />
+					<meta property='og:site_name' content='Yasna Front-End Hiring Challenge' />
+					<meta property='og:url' content={process.env.NEXT_PUBLIC_BASE_URL} />
+					<meta property='og:image' content='/apple-touch-icon.png' />
+
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
@@ -18,5 +56,3 @@ class MyDocument extends Document {
 		)
 	}
 }
-
-export default MyDocument
