fda449f5f3f36eaeb34a16f107d2dbe5f30f7043
Farhad <faradidev@gmail.com>
2020-08-26 15:34:15 +0430

    seo and accessibility related configs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Head from 'next/head';
 import '../styles/index.css';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Component {...pageProps} />
+		<>
+			<Head>
+				<meta
+					name='description'
+					content='A Front-End Hiring Challenge for Yasna Team'
+				/>
+				<meta
+					name='keywords'
+					content='Front End, Front-End, Challenge, Hiring Chanllegne, React, Javascript, Nextjs'
+				/>
+				<link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} />
+			</Head>
+			<Component {...pageProps} />
+		</>
 	)
 }
